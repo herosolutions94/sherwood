63cd489cd9eccd17395f40093a24b43727ee330b
Annaya10 <zf1754611@gmail.com>
2025-03-24 23:50:30 +0500

    Latest Changes

--- pages/_app.js
+++ pages/_app.js
@@ -3,17 +3,31 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../styles/custom.scss'
-import Layout from '../components/layout' 
-'Zoya cghjghjhkjhk';
+import Layout from '../components/layout'
+import http from '@/helpers/http';
+import { doObjToFormData } from '@/helpers/helpers';
 
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps , siteSettings }) {
+
 
   const renderWithLayout =
     Component.getLayout ||
     function (page) {
-      return <Layout>{page}</Layout>;
+      return <Layout siteSettings={siteSettings}>{page}</Layout>;
     };
 
   return renderWithLayout(<Component {...pageProps} />);
+  
 }
+
+
+App.getInitialProps = async ({ ctx }) => {
+  // const cookies = parse(ctx?.req?.headers?.cookie || "");
+  // const authToken = cookies?.authToken || "";
+  const siteSettings = await http
+    .post("site-settings", doObjToFormData({ token: "" }))
+    .then((response) => response?.data?.site_settings)
+    .catch((error) => error?.response?.data?.message);
+  return { siteSettings };
+};
